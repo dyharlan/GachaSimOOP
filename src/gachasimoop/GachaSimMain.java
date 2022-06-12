64d2f19b6799db4aea2901bd2c0f232d6044c673
dyharlan <dyharlan45@gmail.com>
2022-06-12 19:26:55 +0800

    Cleanup Main class

--- src/gachasimoop/GachaSimMain.java
+++ src/gachasimoop/GachaSimMain.java
@@ -17,81 +17,50 @@ public class GachaSimMain {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-        if (args.length > 0)
-            {
-                StringBuilder help = new StringBuilder();
-                help.append("\nThese are the valid arguments for GachaSim: \n");
-                help.append("--cli or -c: Calls the cli frontend.\n");
-                help.append("--javafx or -fx: (Default) Calls the JavaFX-based frontend.\n");
-                String one = args[0];
-                switch (one.toLowerCase())
-                    {
-                        case "--cli":
-                        case "-c":
-                            try{
-                                GachaPool.load5StarCharPool("5starchars");
-                                GachaPool.load4StarCharPool("4starchars");
-                                GachaPool.load5StarWpnPool("5starwpns");
-                                GachaPool.load4StarWpnPool("4starwpns");
-                    //            GachaPool.load5StarCharPool("/mnt/d/Others/src/GachaSimOOP/src/gachasimoop/5starchars");
-                    //            GachaPool.load4StarCharPool("/mnt/d/Others/src/GachaSimOOP/src/gachasimoop/4starchars");
-                    //            GachaPool.load5StarWpnPool("/mnt/d/Others/src/GachaSimOOP/src/gachasimoop/5starwpns");
-                    //            GachaPool.load4StarWpnPool("/mnt/d/Others/src/GachaSimOOP/src/gachasimoop/4starwpns");
-                            }
-                            catch(IOException ioe){
-                                System.err.println(ioe.toString());
-                                System.exit(0);
-                            }
-                            WishSim.cli();
-                            break;
-                        case "--javafx":
-                        case "-fx":
-                            try{
-                                GachaPool.load5StarCharPool("5starchars");
-                                GachaPool.load4StarCharPool("4starchars");
-                                GachaPool.load5StarWpnPool("5starwpns");
-                                GachaPool.load4StarWpnPool("4starwpns");
-                    //            GachaPool.load5StarCharPool("/mnt/d/Others/src/GachaSimOOP/src/gachasimoop/5starchars");
-                    //            GachaPool.load4StarCharPool("/mnt/d/Others/src/GachaSimOOP/src/gachasimoop/4starchars");
-                    //            GachaPool.load5StarWpnPool("/mnt/d/Others/src/GachaSimOOP/src/gachasimoop/5starwpns");
-                    //            GachaPool.load4StarWpnPool("/mnt/d/Others/src/GachaSimOOP/src/gachasimoop/4starwpns");
-                            }
-                            catch(IOException ioe){
-                                System.err.println(ioe.toString());
-                                System.exit(0);
-                            }
-                            WishSimFX.fx();
-                            break;
-                        default:
-                        case "-h":
-                        case "--help":
-                            System.out.print(help);
-                            break;
-                    }
+        try{
+            GachaPool.load5StarCharPool("5starchars");
+            GachaPool.load4StarCharPool("4starchars");
+            GachaPool.load5StarWpnPool("5starwpns");
+            GachaPool.load4StarWpnPool("4starwpns");      
+        }
+        catch(IOException ioe){
+            System.err.println(ioe.toString());
+            System.exit(0);
+        }
+        if (args.length > 0){
+            StringBuilder help = new StringBuilder();
+            help.append("\nThese are the valid arguments for GachaSim: \n");
+            help.append("--cli or -c: Calls the cli frontend.\n");
+            help.append("--javafx or -fx: (Default) Calls the JavaFX-based frontend.\n");
+            String one = args[0];
+            switch (one.toLowerCase()){
+                case "--cli":
+                case "-c":
+                    WishSim.cli();
+                    break;
+                case "--javafx":
+                case "-fx":
+                    callFX();
+                    break;
+                default:
+                case "-h":
+                case "--help":
+                    System.out.print(help);
+                    break;
             }
+        }
         else
-            try{
-                    GachaPool.load5StarCharPool("5starchars");
-                    GachaPool.load4StarCharPool("4starchars");
-                    GachaPool.load5StarWpnPool("5starwpns");
-                    GachaPool.load4StarWpnPool("4starwpns");
-                    //            GachaPool.load5StarCharPool("/mnt/d/Others/src/GachaSimOOP/src/gachasimoop/5starchars");
-                    //            GachaPool.load4StarCharPool("/mnt/d/Others/src/GachaSimOOP/src/gachasimoop/4starchars");
-                    //            GachaPool.load5StarWpnPool("/mnt/d/Others/src/GachaSimOOP/src/gachasimoop/5starwpns");
-                    //            GachaPool.load4StarWpnPool("/mnt/d/Others/src/GachaSimOOP/src/gachasimoop/4starwpns");
-                }
-                catch(IOException ioe){
-                    System.err.println(ioe.toString());
-                    System.exit(0);
-                }
-            try{
-                WishSimFX.fx();
-            }
-            catch(Exception ex){
-                WishSim.cli();
-            }
-       
+            callFX();
+    }
+    
+    public static void callFX(){
+        try{
+            WishSimFX.fx();
+        }
+        catch(Exception | Error ex){
+            System.err.println("JavaFX Libraries Not Found, falling back to cli...");
+            WishSim.cli();
+        } 
     }
     
 }
